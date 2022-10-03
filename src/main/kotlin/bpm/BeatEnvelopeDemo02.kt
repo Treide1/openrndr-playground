03cd334c556a0d765f8a33aedc3885273c889c46
Treide1 <62839435+Treide1@users.noreply.github.com>
2022-10-03 21:03:21 +0200

    clarified docs for BeatEnvelopeDemo02

--- src/main/kotlin/bpm/BeatEnvelopeDemo02.kt
+++ src/main/kotlin/bpm/BeatEnvelopeDemo02.kt
@@ -23,6 +23,14 @@ fun main() = application {
         val bpm = 121.0 // Song: "Step-Grandma" by Salvatore Ganacci
         val sampleCount = 40
 
+        val lowerY = height*.9
+        val upperY = height*.1
+        val leftX = width*.2
+        val rightX = width*.8
+        val rad = 10.0
+        val leftColor = ColorRGBa.GREEN
+        val rightColor = ColorRGBa.RED
+
         // Creating BeatEnvelopes and adding them to clock
         val lowFreqSine = constructSine(bpm, 4, 4.0)
         val highFreqSine = constructSine(bpm, 4, 1.0, .4)
@@ -32,19 +40,13 @@ fun main() = application {
             add(highFreqSine)
         }
 
+        // Calculating weights in advance
         val decreasingWeights = interpolationList(0.0, 1.0, sampleCount).map { 1.0 - it }
         val increasingWeights = interpolationList(0.0, 1.0, sampleCount).map { it }
 
         extend {
 
-            val lowerY = height*.9
-            val upperY = height*.1
-            val leftX = width*.2
-            val rightX = width*.8
-            val rad = 10.0
-            val leftColor = ColorRGBa.GREEN
-            val rightColor = ColorRGBa.RED
-
+            // Weighted sum of envelope samples
             val pastLFO = lowFreqSine.sampleList(0.0, 1.0, sampleCount)
             val pastHFO = highFreqSine.sampleList(0.0, 1.0, sampleCount)
 
@@ -53,12 +55,14 @@ fun main() = application {
 
             val mixedSamples = weightedLFO add weightedHFO
 
+            // Comparison circle left
             drawer.isolated {
                 fill = leftColor
                 val x = (0 + leftX) * .5
                 val y = lowFreqSine.sample().map(0.0, 1.0, lowerY, upperY)
                 circle(x, y, rad)
             }
+            // Comparison circle right
             drawer.isolated {
                 fill = rightColor
                 val x = (width + rightX) * .5
@@ -66,6 +70,7 @@ fun main() = application {
                 circle(x, y, rad)
             }
 
+            // Interpolation circles
             drawer.isolated {
                 mixedSamples.forEachIndexed { index, sample ->
                     val frac = index.toDouble().map(0.0, mixedSamples.lastIndex.toDouble(), 0.0, 1.0)
