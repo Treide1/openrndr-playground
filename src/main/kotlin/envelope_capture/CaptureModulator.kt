76156f25def658a8577e455e696a7b6e499c2943
lukas <lukas.henke.berlin@gmx.de>
2022-10-30 15:07:10 +0100

    added capture modulator synopsis

---
+++ src/main/kotlin/envelope_capture/CaptureModulator.kt
@@ -0,0 +1,9 @@
+package envelope_capture
+
+/**
+ * Introduce and modulate four envelopes,
+ * that are individually captured by mouse movement.
+ *
+ * Every track is displayed as a left-moving line.
+ * The modulation can be toggled for each curve.
+ */
