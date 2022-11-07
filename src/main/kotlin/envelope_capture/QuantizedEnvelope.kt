e3176e0d04a4d4c985ae6489bf5d7b4c9aa7e3bd
lukas <lukas.henke.berlin@gmx.de>
2022-11-07 09:56:06 +0100

    added sod visualization

--- src/main/kotlin/envelope_capture/QuantizedEnvelope.kt
+++ src/main/kotlin/envelope_capture/QuantizedEnvelope.kt
@@ -7,10 +7,14 @@ import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.isolated
 import org.openrndr.extra.color.presets.*
+import org.openrndr.extra.shadestyles.linearGradient
+import org.openrndr.math.Vector2
 import org.openrndr.math.map
+import org.openrndr.shape.contour
 import utils.map
 import utils.vh
 import utils.vw
+import kotlin.math.pow
 
 fun main() = application {
     configure {
@@ -49,13 +53,66 @@ fun main() = application {
             }
         }
 
+        val n = 30
+        val fac = .99
+        val settle = .25
+        val x0 = vw(.75 - .15)
+        val x1 = vw(.75 + .15)
+        val y0 = vh(.2)
+        val y1 = vh(.8)
+        val waveList = MutableList(n) {0.0}
+        val waveWeights = List(n) {fac.pow(it)}
+        extend {
+            waveList.add(0, sod.y)
+            waveList.removeLast()
+
+            val c = contour {
+                waveList.forEachIndexed { i, v ->
+                    val relV = v.map(-1.0, 3.0, 0.0, 1.0)
+                    val x = i.map(0, n-1, x0, x1)
+                    val y = ((relV - settle) * waveWeights[i] + settle).map(0.0, 1.0, y0, y1)
+
+                    moveOrLineTo(x, y)
+                }
+
+                val settleY = vh(settle.map(0.0, 1.0, .2, .8))
+                lineTo(x1, settleY)
+                lineTo(x0, settleY)
+                close()
+            }
+
+            val baseCol = ColorRGBa.PINK.mix(ColorRGBa.PURPLE, sod.y*.5)
+            drawer.isolated {
+                shadeStyle = linearGradient(baseCol, ColorRGBa.BLACK,
+                    offset = Vector2(.1, 0.0), rotation = -80.0)
+                stroke = null
+
+                contour(c)
+            }
+        }
+
+        val textLines = listOf("Controls", "q: To 1", "no key press: 0", "w: to -1", "e: to -2", "r to -3")
+        extend {
+
+            drawer.isolated {
+                val x = 20.0
+                var y = 40.0
+                val yOff = 30.0
+                stroke = ColorRGBa.WHITE
+                fill = ColorRGBa.WHITE
+                textLines.forEach {
+                    text(it, x, y)
+                    y+=yOff
+                }
+
+            }
+        }
 
         keyboard.keyDown.listen {
             when(it.key) {
                 KEY_ESCAPE -> application.exit()
             }
         }
-
 
     }
 }
