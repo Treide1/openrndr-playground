d210d42480d17dc504905edd6ff5daed1535294e
lukas <lukas.henke.berlin@gmx.de>
2023-02-12 21:58:02 +0100

    refactoring, support for accumulation buffer

--- src/main/kotlin/playground/IterativeImageWarping.kt
+++ src/main/kotlin/playground/IterativeImageWarping.kt
@@ -2,7 +2,10 @@ package playground
 
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
+import org.openrndr.draw.ColorBufferShadow
 import org.openrndr.draw.renderTarget
+import org.openrndr.math.Vector2
+import kotlin.system.measureTimeMillis
 
 
 /**
@@ -31,31 +34,38 @@ import org.openrndr.draw.renderTarget
  * Should we encounter a G(x_s) with eigenvalues only greater 1 in absolute value,
  * then the original pixel x_w needs to be interpolated somehow.
  * As we are in 2D, we only have to deal with a 2x2 matrix A = ((a b)(c d)).
- * The eigenvectors are v with lv = Av with eigenvalue l.
- *      <=> (A-lI)v = 0
- * eig(A): det ((a-l b)(c d-l)) = 0 <=> (a-l)*(d-l) - bc = det A - l*trace A + l^2 = 0
- *      <=> ... [See clever people's algorithm]
- *
- * If all |l| > 1 resolve differently.
+ * The eigenvalues of A for trace T = a+d, and determinant D = ad - bc are
+ * l_1 = T/2 + (T^2/4-D)^1/2 ,
+ * l_2 = T/2 - (T^2/4-D)^1/2 .
+ * If all |l| > 1 are calculated, interpolate accordingly.
  */
 
+// Globals
+val w = 640
+val h = 480
+
+val center = Vector2(w/2.0, h/2.0)
+val size = w*h
+
 fun main() = application {
-    val w = 640
-    val h = 480
-    val size = w*h
+
+    // Convenience funcs
+    fun Vector2.isInViewport(): Boolean = (x.toInt() in 0 until w) && (y.toInt() in 0 until h)
+    fun Int.toPixelVec() : Vector2 {
+        return Vector2((this % w).toDouble(), (this / w).toDouble()) // .also { println("toInt2d: $this -> $it") }
+    }
+
+    // Double panel viewport:
+    // Left side is original, right side is warp result
     configure {
-        width=2*w
-        height=h
+        this.width=2*w
+        this.height=h
     }
     program {
         // Warp func
-        val V = { x: Int, y:Int  ->
-            val xN = (w-x)/2
-            val yN = (h-y)/2
-            xN + yN*w
-        }
-        fun G(x_w: Int): (Int) -> Int = { x_s: Int ->
-            x_w - V(x_s%w, x_s/w)
+        fun getV() : (Vector2) -> Vector2 = { p:Vector2 ->
+            val ang = mouse.position.y / h * 90.0
+            (center - p).rotate(ang)
         }
 
         val rt = renderTarget(w, h) {
@@ -65,51 +75,66 @@ fun main() = application {
         }
         val sourceShadow = rt.colorBuffer(0).shadow
         val warpedShadow = rt.colorBuffer(1).shadow
+        val accumulatedShadow = rt.colorBuffer(2).shadow
+        accumulatedShadow.forEachIndex { i ->
+            accumulatedShadow[i%w, i/w] = ColorRGBa.BLACK
+        }
 
-        val delta_x = -100
-        val max_iter = 20
-        fun getSourcePixelColor(x_w: Int) : ColorRGBa {
+        val delta_x = Vector2(-100.0, 0.0)
+        val max_iter = 4
+        fun getSourcePixelColor(x_w: Vector2) : ColorRGBa {
             // Preprocess contraction
-            val contraction = G(x_w)
+            val warp = getV()
             // Pick x_0
-            var currentX = x_w+delta_x
+            var currentX = x_w + delta_x
+            // Iterate over contraction
             repeat(max_iter) {
-                currentX = contraction(currentX)
+                currentX = x_w - warp(currentX) // = fix point function
             }
-            var x_s = currentX
-            if (x_s < 0 || x_s >= size) x_s = -1
-            return if (x_s != -1) sourceShadow[x_s%w, x_s/w] else ColorRGBa.BLACK
+            // Use source pixel
+            val x_s = currentX
+            return if (x_s.isInViewport()) sourceShadow[x_s] else ColorRGBa.BLACK
         }
 
         extend {
-            // Interactive blending value
-            val relAlpha = mouse.position.y / h
+            measureTimeMillis{
+                // Draw on raw
+                val blue = mouse.position.x / w
+                sourceShadow.forEachIndex { i_s ->
+                    val x = i_s % w
+                    val y = i_s / w
+                    sourceShadow[x, y] = ColorRGBa(x.toDouble() / w, y.toDouble() / h, blue)
+                }
+                sourceShadow.upload()
 
-            // Draw on raw
-            sourceShadow.download()
-            for (x_s in 0 until size) {
-                val x = x_s % w
-                val y = x_s / w
-                sourceShadow[x, y] = ColorRGBa(x.toDouble()/w, y.toDouble()/h, 1.0)
-            }
-            sourceShadow.upload()
+                // Draw on warped
+                warpedShadow.forEachIndex { i_w ->
+                    val x_w = i_w.toPixelVec()
+                    val c = getSourcePixelColor(x_w)
+                    warpedShadow[x_w] = c
+                }
 
-            // Draw on warped
-             warpedShadow.download()
-            for (x_w in 0 until size) {
-                val x = x_w % w
-                val y = x_w / w
-                val c = getSourcePixelColor(x_w)
-                warpedShadow[x, y] = c
-            }
-            warpedShadow.upload()
+                accumulatedShadow.forEachIndex { i_a ->
+                    val c = warpedShadow[i_a%w, i_a/w]
+                    accumulatedShadow[i_a%w, i_a/w] = c*0.5 + accumulatedShadow[i_a%w, i_a/w] * 0.98
+                }
 
-            drawer.image(rt.colorBuffer(0), 0.0, 0.0)
-            drawer.image(rt.colorBuffer(1), w.toDouble(), 0.0)
+                drawer.image(rt.colorBuffer(0), 0.0, 0.0)
+                drawer.image(rt.colorBuffer(2), w.toDouble(), 0.0)
+            }.also { println("time per frame: $it ms") }
         }
     }
 }
 
-// TODO: refactor to use this data class to do unambiguous maths
-data class Pixel(val x: Int, val y: Int)
+inline fun ColorBufferShadow.forEachIndex(block: (Int) -> Unit) {
+    this.download()
+    for(i in 0 until size) { block(i) }
+    this.upload()
+}
+private operator fun ColorBufferShadow.get(v: Vector2) : ColorRGBa {
+    return this[v.x.toInt(), v.y.toInt()]
+}
 
+private operator fun ColorBufferShadow.set(v: Vector2, color: ColorRGBa) {
+    this[v.x.toInt(), v.y.toInt()] = color
+}
