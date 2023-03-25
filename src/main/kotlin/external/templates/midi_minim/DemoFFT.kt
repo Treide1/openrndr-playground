2b459cd37109e70ce17ecf94f909cd9c866c28e7
lh1906s <lukas.henke@alumni.fh-aachen.de>
2023-03-25 16:38:22 +0100

    updated fft demo with comments

--- src/main/kotlin/external/templates/midi_minim/DemoFFT.kt
+++ src/main/kotlin/external/templates/midi_minim/DemoFFT.kt
@@ -7,22 +7,31 @@ import ddf.minim.analysis.LanczosWindow
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.extra.minim.minim
+import org.openrndr.math.IntVector2
 import utils.lerp
 import kotlin.math.ln
 
 fun main() {
     application {
         configure {
-            width = 1280
-            height = 720
+            width = 640
+            height = 480
+            position = IntVector2.ZERO
         }
 
         program {
+            // Setup minim
             val minim = minim()
+            // Uses system standard audio input device.
+            // This depends on system settings (usually your mic).
+            // To use application audio output, you have to route via your system.
+            // For Windows, see "Stereo Mix". For macOS, see "Blackhole".
             val lineIn = minim.getLineIn(Minim.MONO, 2048, 48000f)
+            // FFT setup with config
             val fft = FFT(lineIn.bufferSize(), lineIn.sampleRate())
             fft.window(LanczosWindow())
 
+            // Drawing (for illustration purposes)
             val n = fft.specSize().div(4)
 
             val w = width * 0.8
@@ -31,8 +40,10 @@ fun main() {
             val firstX = (width-w)/2
 
             extend {
+                // Feed the fft with the latest audio data
                 fft.forward(lineIn.mix)
 
+                // Draw the fft result
                 for (i in 0 until n) {
                     val freq = fft.getBand(i)
                     val freqCount = fft.timeSize() // same as bufferSize
