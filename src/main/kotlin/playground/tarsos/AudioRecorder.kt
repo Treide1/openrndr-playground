c52bcfa45c1f21db36b4076aa90007a08b2bb309
lh1906s <lukas.henke@alumni.fh-aachen.de>
2023-04-07 20:23:32 +0200

    prepared AudioRecorder.kt using TarsosDSP

---
+++ src/main/kotlin/playground/tarsos/AudioRecorder.kt
@@ -0,0 +1,17 @@
+package playground.tarsos
+
+import org.openrndr.application
+
+/**
+ * Uses TarsosDSP to write audio to a WAV file.
+ * Uses WaveformWriter to capture and write the audio.
+ * Press SPACEBAR to start/stop recording.
+ */
+fun main() = application {
+    configure { }
+    program {
+        extend {
+
+        }
+    }
+}
