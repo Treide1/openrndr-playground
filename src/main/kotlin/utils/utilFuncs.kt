1977cf3c5d94e9ee9923b7185befe02663891260
lh1906s <lukas.henke@alumni.fh-aachen.de>
2023-04-18 12:06:26 +0200

    added CyclicFlag util

--- src/main/kotlin/utils/utilFuncs.kt
+++ src/main/kotlin/utils/utilFuncs.kt
@@ -431,3 +431,20 @@ fun <T: Any> T.alsoWrite(writer: CsvWriter, endOfLine: Boolean = false, transfor
     writer.write(s, endOfLine)
     return this
 }
+
+/**
+ * Data class for storing [options] and cycling through them with [next].
+ * Initial value is the first element of [options]. Repeats after the last one.
+ */
+class CyclicFlag<T>(val options: List<T>) {
+    var index = 0
+    private var _value = options[index]
+
+    val value: T
+        get() = _value
+
+    fun next() {
+        index = (index + 1) % options.size
+        _value = options[index]
+    }
+}
